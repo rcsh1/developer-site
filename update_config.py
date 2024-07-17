9fd721ea1ad0af44a54f34c8caf99b777617861a
王强 <wangqiang@wqmac.local>
2024-07-17 17:25:28 +0800

    update waas2 content

--- update_config.py
+++ update_config.py
@@ -2,6 +2,24 @@ import subprocess
 import json
 import sys
 import traceback
+import os
+import shutil
+
+
+def delete_exist_files():
+    folders_to_delete = [
+        'v2/api-references/developers--webhooks',
+        'v2/api-references/transactions',
+        'v2/api-references/wallets',
+        'v2/api-references/wallets--mpc-wallets',
+    ]
+
+    for folder in folders_to_delete:
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+            print(f'已删除: {folder}')
+        else:
+            print(f'文件夹不存在: {folder}')
 
 
 def process_spec_data(original_spec_data):
@@ -15,13 +33,12 @@ def update():
     try:
         source_file = "v2/cobo_waas2_openapi_spec/dev_openapi.yaml"
         destination_dir = "v2/api-references/"
-        api_file = f"{destination_dir}/dev_openapi.yaml"
         mint_file = "mint.json"
 
-        subprocess.run(["cp", source_file, destination_dir], check=True)
+        delete_exist_files()
 
         command = [
-            "npx", "@mintlify/scraping@latest", "openapi-file", api_file, "-o", destination_dir
+            "npx", "@mintlify/scraping@latest", "openapi-file", source_file, "-o", destination_dir
         ]
         original_spec_data = subprocess.run(command, capture_output=True, text=True).stdout.split('\n', 1)[1]
 
